22cb797e94d6cd98949432f510a3d14dbd78bc86
BlueEagle421 <joachim.wielewski@gmail.com>
2025-07-25 12:38:34 +0200

    Add fuel to telepad

--- Source/Comps/CompTelepad.cs
+++ Source/Comps/CompTelepad.cs
@@ -8,10 +8,9 @@ namespace USH_GE;
 
 public class CompProperties_Telepad : CompProperties_Interactable
 {
-    public int fuelConsumption;
-    public HediffDef hediffDefToRemove;
-    public FleckDef fleckDef;
-    public SoundDef soundDef;
+    public int fuelConsumption = 5;
+    public HediffDef hediffDef = USH_DefOf.USH_TelepadNausea;
+    public float hediffAddChance = 0.07f;
 
     public CompProperties_Telepad() => compClass = typeof(CompTelepad);
 }
@@ -19,9 +18,10 @@ public class CompProperties_Telepad : CompProperties_Interactable
 [StaticConstructorOnStartup]
 public class CompTelepad : CompInteractable, ITargetingSource
 {
-    public CompProperties_Telepad ModuleProps => (CompProperties_Telepad)props;
+    public CompProperties_Telepad PadProps => (CompProperties_Telepad)props;
     private readonly TargetingParameters TargetingParameters;
     private List<Pawn> _teleportSequence;
+    CompRefuelable _refuelableComp;
 
     public CompTelepad()
     {
@@ -31,6 +31,12 @@ public class CompTelepad : CompInteractable, ITargetingSource
             canTargetSelf = false,
             canTargetBuildings = false,
         };
+    }
+
+    public override void PostSpawnSetup(bool respawningAfterLoad)
+    {
+        base.PostSpawnSetup(respawningAfterLoad);
+        _refuelableComp = parent.GetComp<CompRefuelable>();
     }
 
     public override void CompTick()
@@ -59,6 +65,8 @@ public class CompTelepad : CompInteractable, ITargetingSource
         TryToGiveNausea(toTel);
 
         Interact(toTel, true);
+
+        _refuelableComp.ConsumeFuel(PadProps.fuelConsumption);
 
         SoundDefOf.Psycast_Skip_Entry.PlayOneShot(parent);
         SkipUtility.SkipTo(toTel, parent.Position, parent.Map);
@@ -118,6 +126,14 @@ public class CompTelepad : CompInteractable, ITargetingSource
             return interactionReport.Reason;
 
         return true;
+    }
+
+    public override AcceptanceReport CanInteract(Pawn activateBy = null, bool checkOptionalItems = true)
+    {
+        if (_refuelableComp.Fuel < PadProps.fuelConsumption)
+            return "NoFuel";
+
+        return base.CanInteract(activateBy, checkOptionalItems);
     }
 
     public override IEnumerable<Gizmo> CompGetGizmosExtra()
