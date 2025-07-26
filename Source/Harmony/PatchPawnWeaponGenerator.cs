c8269eb22b68d01b7e34a18c507a384795df832a
BlueEagle421 <joachim.wielewski@gmail.com>
2025-07-26 22:53:03 +0200

    Improve weapon patch

--- Source/Harmony/PatchPawnWeaponGenerator.cs
+++ Source/Harmony/PatchPawnWeaponGenerator.cs
@@ -11,12 +11,11 @@ public static class Patch_PawnWeaponGenerator_TryGenerateWeaponFor
     private const float OVERCLOCK_CHANCE = 0.08f;
     public static void Postfix(Pawn pawn, PawnGenerationRequest request)
     {
-        var weapon = pawn.equipment?.Primary;
-        if (weapon is ThingWithComps thing)
-        {
-            if (thing.TryGetComp(out CompOverclock compOverclock))
-                if (Rand.Chance(OVERCLOCK_CHANCE) || request.KindDef == USH_DefOf.USH_AncientGlittertechSoldier)
-                    compOverclock.IsOverclocked = true;
-        }
+        if (pawn?.equipment?.Primary is not ThingWithComps thing)
+            return;
+
+        if (thing.TryGetComp(out CompOverclock compOverclock))
+            if (Rand.Chance(OVERCLOCK_CHANCE) || request.KindDef == USH_DefOf.USH_AncientGlittertechSoldier)
+                compOverclock.IsOverclocked = true;
     }
 }
