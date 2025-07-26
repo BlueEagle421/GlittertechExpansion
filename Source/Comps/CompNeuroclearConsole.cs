1b2e0dfdbc4798e942f783e805adf9b201bd1f33
BlueEagle421 <joachim.wielewski@gmail.com>
2025-07-26 15:54:23 +0200

    Rework neuroclear console

--- Source/Comps/CompNeuroclearConsole.cs
+++ Source/Comps/CompNeuroclearConsole.cs
@@ -1,9 +1,43 @@
 ﻿using RimWorld;
 using System.Collections.Generic;
+using UnityEngine;
 using Verse;
 using Verse.Sound;
 
 namespace USH_GE;
+
+public class PlaceWorker_NeuroclearConsole : PlaceWorker
+{
+    public override void DrawGhost(ThingDef def, IntVec3 center, Rot4 rot, Color ghostCol, Thing thing = null)
+    {
+        Map currentMap = Find.CurrentMap;
+        Room room = center.GetRoom(currentMap);
+
+        if (room == null)
+            return;
+
+        if (!room.ProperRoom)
+            return;
+
+        Color egesColor = WillWork(room, def) ? ColorLibrary.BabyBlue : Color.red;
+
+        GenDraw.DrawFieldEdges([.. room.Cells], egesColor, null);
+    }
+
+    private bool WillWork(Room room, ThingDef thingDef)
+    {
+        if (room == null)
+            return false;
+
+        if (room.CellCount > thingDef.GetCompProperties<CompProperties_NeuroclearConsole>().maxRoomCellSize)
+            return false;
+
+        if (room.OpenRoofCount != 0)
+            return false;
+
+        return true;
+    }
+}
 
 public class CompProperties_NeuroclearConsole : CompProperties_Interactable
 {
@@ -11,6 +45,7 @@ public class CompProperties_NeuroclearConsole : CompProperties_Interactable
     public HediffDef hediffDefToRemove;
     public FleckDef fleckDef;
     public SoundDef soundDef;
+    public int maxRoomCellSize;
 
     public CompProperties_NeuroclearConsole() => compClass = typeof(CompNeuroclearConsole);
 }
@@ -18,7 +53,6 @@ public class CompProperties_NeuroclearConsole : CompProperties_Interactable
 public class CompNeuroclearConsole : CompInteractable
 {
     Map _currentMap;
-    CompFacility _facilityComp;
     CompRefuelable _refuelableComp;
 
     public CompProperties_NeuroclearConsole ModuleProps => (CompProperties_NeuroclearConsole)props;
@@ -27,7 +61,6 @@ public class CompNeuroclearConsole : CompInteractable
     {
         base.PostSpawnSetup(respawningAfterLoad);
         _currentMap = parent.Map;
-        _facilityComp = parent.GetComp<CompFacility>();
         _refuelableComp = parent.GetComp<CompRefuelable>();
     }
 
@@ -40,13 +73,10 @@ public class CompNeuroclearConsole : CompInteractable
 
     protected override bool TryInteractTick()
     {
-        if (_facilityComp.LinkedBuildings.Count == 0)
-            return false;
-
         if (_refuelableComp.Fuel < ModuleProps.fuelConsumption)
             return false;
 
-        if (PawnsInLinkedFacilities().Count == 0)
+        if (PawnsToDesensitize().Count == 0)
             return false;
 
         return true;
@@ -54,7 +84,7 @@ public class CompNeuroclearConsole : CompInteractable
 
     private void DesensitizePawns()
     {
-        foreach (Pawn pawn in PawnsInLinkedFacilities())
+        foreach (Pawn pawn in PawnsToDesensitize())
         {
             Hediff toRemove = pawn.health.hediffSet.GetFirstHediffOfDef(ModuleProps.hediffDefToRemove);
 
@@ -69,26 +99,49 @@ public class CompNeuroclearConsole : CompInteractable
             SpawnFleckEffect(pawn.Position);
             SpawnFleckEffect(parent.Position);
         }
+
+        float fleckOnCellChance = 0.35f;
+
+        foreach (var cell in RoomCells)
+            if (Rand.Chance(fleckOnCellChance))
+                SpawnFleckEffect(cell);
     }
 
-    private void SpawnFleckEffect(IntVec3 position) => FleckMaker.Static(position, _currentMap, ModuleProps.fleckDef);
+    private void SpawnFleckEffect(IntVec3 position) => FleckMaker.Static(position, _currentMap, ModuleProps.fleckDef, Rand.Range(0.8f, 1.25f));
 
     private void PlaySoundEffect() => ModuleProps.soundDef.PlayOneShot(new TargetInfo(parent.Position, parent.Map, false));
 
-    private List<Pawn> PawnsInLinkedFacilities()
+    private IEnumerable<IntVec3> RoomCells
+        => parent.GetRoom().Cells;
+
+    private List<Pawn> PawnsToDesensitize()
     {
         List<Pawn> result = [];
 
-        foreach (Thing building in _facilityComp.LinkedBuildings)
-            foreach (Thing thing in _currentMap.thingGrid.ThingsAt(building.Position))
-                if (thing as Pawn != null)
-                    result.Add(thing as Pawn);
+        foreach (var cell in RoomCells)
+            foreach (Thing thing in _currentMap.thingGrid.ThingsAt(cell))
+                if (thing is Pawn pawn)
+                    result.Add(pawn);
 
         return result;
     }
 
     public override AcceptanceReport CanInteract(Pawn activateBy = null, bool checkOptionalItems = true)
     {
+        Room currentRoom = parent.GetRoom();
+
+        if (currentRoom == null)
+            return "USH_GE_NoRoom".Translate();
+
+        if (parent.GetRoom().CellCount > ModuleProps.maxRoomCellSize)
+            return "USH_GE_RoomTooBig".Translate(ModuleProps.maxRoomCellSize);
+
+        if (parent.GetRoom().OpenRoofCount != 0)
+            return "USH_GE_RoomUnroofed".Translate();
+
+        if (PawnsToDesensitize().NullOrEmpty())
+            return "USH_GE_NoAnesthesia".Translate();
+
         if (_refuelableComp.Fuel < ModuleProps.fuelConsumption)
             return "NoFuel".Translate();
 
