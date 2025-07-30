81a522b864834fb61583d4b6bd6c56de1ea96c25
BlueEagle421 <joachim.wielewski@gmail.com>
2025-07-30 15:08:07 +0200

    Add stats displaying in upgrade lenses

--- Source/Comps/CompOverclockUpgrade.cs
+++ Source/Comps/CompOverclockUpgrade.cs
@@ -1,4 +1,7 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using RimWorld;
 using Verse;
 
@@ -23,4 +26,28 @@ public class CompOverclockUpgrade : ThingComp
     {
         base.PostSpawnSetup(respawningAfterLoad);
     }
+
+    public override IEnumerable<StatDrawEntry> SpecialDisplayStats()
+    {
+        StringBuilder stringBuilder = new();
+        stringBuilder.AppendLine("Stat_ThingUniqueWeaponTrait_Desc".Translate());
+        stringBuilder.AppendLine();
+        List<StatModifier> allMods = [];
+
+        StatCategoryDef categoryDef = StatCategoryDefOf.EquippedStatOffsets;
+
+        if (!Props.statOffsets.NullOrEmpty())
+            foreach (var mod in Props.statOffsets)
+            {
+                string content = mod.stat.Worker.ValueToString(mod.value, finalized: false, ToStringNumberSense.Factor);
+                yield return new StatDrawEntry(categoryDef, mod.stat.LabelCap, content, mod.stat.description, 1104);
+            }
+        if (!Props.statFactors.NullOrEmpty())
+            foreach (var mod in Props.statFactors)
+            {
+                string content = mod.stat.Worker.ValueToString(mod.value, finalized: false, ToStringNumberSense.Factor);
+                yield return new StatDrawEntry(categoryDef, mod.stat.LabelCap, content, mod.stat.description, 1104);
+            }
+    }
+
 }
