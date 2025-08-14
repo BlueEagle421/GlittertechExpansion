fff889a40d817d656062327def629379b97f7141
BlueEagle421 <joachim.wielewski@gmail.com>
2025-08-14 11:46:37 +0200

    Fix tolerance patch

--- Source/Harmony/PatchModifyChemicalEffectForToleranceAndBodySize.cs
+++ Source/Harmony/PatchModifyChemicalEffectForToleranceAndBodySize.cs
@@ -7,8 +7,15 @@ namespace USH_GE;
 [HarmonyPatch(typeof(AddictionUtility), nameof(AddictionUtility.ModifyChemicalEffectForToleranceAndBodySize))]
 static class Patch_AddictionUtility_ModifyChemicalEffectForToleranceAndBodySize
 {
-    static void Postfix(Pawn pawn, ChemicalDef chemicalDef, ref float effect, bool applyGeneToleranceFactor, bool divideByBodySize = true)
+    static void Postfix(Pawn pawn, ref float effect)
     {
-        effect *= pawn.GetStatValue(USH_DefOf.USH_DrugToleranceGainRate);
+        if (pawn == null)
+            return;
+
+        try
+        {
+            effect *= pawn.GetStatValue(USH_DefOf.USH_DrugToleranceGainRate);
+        }
+        catch { }
     }
 }
