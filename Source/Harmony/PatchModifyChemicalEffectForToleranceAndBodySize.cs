ae75c8928b0203076e5bca8fba88d74c7408e6e5
BlueEagle421 <joachim.wielewski@gmail.com>
2025-09-16 13:45:31 +0200

    Fix AddictionUtility patch

--- Source/Harmony/PatchModifyChemicalEffectForToleranceAndBodySize.cs
+++ Source/Harmony/PatchModifyChemicalEffectForToleranceAndBodySize.cs
@@ -7,14 +7,15 @@ namespace USH_GE;
 [HarmonyPatch(typeof(AddictionUtility), nameof(AddictionUtility.ModifyChemicalEffectForToleranceAndBodySize))]
 static class Patch_AddictionUtility_ModifyChemicalEffectForToleranceAndBodySize
 {
-    static void Postfix(Pawn pawn, ref float effect)
+    static void Postfix(Pawn pawn, ref float effect, bool applyGeneToleranceFactor)
     {
         if (pawn == null)
             return;
 
         try
         {
-            effect *= pawn.GetStatValue(USH_DefOf.USH_DrugToleranceGainRate);
+            if (applyGeneToleranceFactor)
+                effect *= pawn.GetStatValue(USH_DefOf.USH_DrugToleranceGainRate);
         }
         catch { }
     }
